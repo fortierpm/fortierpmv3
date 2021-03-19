6e03ddd495a76e277f8d9e7ef305e92c1b80baa2
Peter <46471156+fortierpm@users.noreply.github.com>
2021-03-18 21:36:40 -0500

    Small-Width Nav Dropdown

--- app.js
+++ app.js
@@ -47,6 +47,23 @@ window.addEventListener("scroll", () => {
     }
 });
 
+//* Dropdown on small-width screen
+const hamburger = document.querySelector(".hamburger");
+const sitePages = document.querySelector(".site-pages");
+const sitePage = document.querySelectorAll(".site-page");
+const pageLink = document.querySelectorAll(".page-link");
+let burgerToggle = true;
+hamburger.addEventListener("click", () => {
+    if (burgerToggle) {
+        sitePages.classList.add("active-site-pages");
+        hamburger.classList.add("active-burger");
+    } else {
+        sitePages.classList.remove("active-site-pages");
+        hamburger.classList.remove("active-burger");
+    }
+    burgerToggle = !burgerToggle;
+});
+
 //* Hide Contact button
 const contact = document.querySelector(".contact");
 const footer = document.querySelector("footer");
